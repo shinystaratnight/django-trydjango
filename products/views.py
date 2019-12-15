fda389ef013d0dd1604315fb3ce232384f88b7bc
Rising Star <star-atnight@outlook.com>
2019-12-15 18:13:56 +0800

    Add Pure Django Form

--- products/views.py
+++ products/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
-from .forms import ProductForm
+from .forms import ProductForm, RawProductForm
 from .models import Product
 
 def product_create_view(request):
-    # print(request.GET)
-    # print(request.POST)
+    my_form = RawProductForm()
     if request.method == "POST":
-        my_new_title = request.POST.get('title')
-        print(my_new_title)
-        # Product.objects.create(title=my_new_title)
-    context = {}
+        my_form = RawProductForm(request.POST)
+        if my_form.is_valid():
+            # now the data is good
+            print(my_form.cleaned_data)
+            Product.objects.create(**my_form.cleaned_data)
+        else:
+            print(my_form.errors)
+    context = {
+        'form': my_form
+    }
     return render(request, "products/product_create.html", context)
 
 # def product_create_view(request):

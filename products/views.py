cdd9dd96fb234f232f786e9335d53cda41070aef
Rising Star <star-atnight@outlook.com>
2019-12-15 18:44:46 +0800

    add Form Validation

--- products/views.py
+++ products/views.py
@@ -2,31 +2,31 @@ from django.shortcuts import render
 from .forms import ProductForm, RawProductForm
 from .models import Product
 
-def product_create_view(request):
-    my_form = RawProductForm()
-    if request.method == "POST":
-        my_form = RawProductForm(request.POST)
-        if my_form.is_valid():
-            # now the data is good
-            print(my_form.cleaned_data)
-            Product.objects.create(**my_form.cleaned_data)
-        else:
-            print(my_form.errors)
-    context = {
-        'form': my_form
-    }
-    return render(request, "products/product_create.html", context)
-
 # def product_create_view(request):
-#     form = ProductForm(request.POST or None)
-#     if form.is_valid():
-#         form.save()
-#         form=ProductForm()
-
+#     my_form = RawProductForm()
+#     if request.method == "POST":
+#         my_form = RawProductForm(request.POST)
+#         if my_form.is_valid():
+#             # now the data is good
+#             print(my_form.cleaned_data)
+#             Product.objects.create(**my_form.cleaned_data)
+#         else:
+#             print(my_form.errors)
 #     context = {
-#         'form': form
+#         'form': my_form
 #     }
 #     return render(request, "products/product_create.html", context)
+
+def product_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form=ProductForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, "products/product_create.html", context)
 
 def product_detail_view(request):
     obj = Product.objects.get(id=1)

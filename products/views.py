437b94ea92b6649260ec945f2dfad3870d1ddb0c
Rising Star <star-atnight@outlook.com>
2019-12-15 17:59:00 +0800

    Add Raw HTML Form

--- products/views.py
+++ products/views.py
@@ -3,15 +3,25 @@ from .forms import ProductForm
 from .models import Product
 
 def product_create_view(request):
-    form = ProductForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        form=ProductForm()
-
-    context = {
-        'form': form
-    }
+    # print(request.GET)
+    # print(request.POST)
+    if request.method == "POST":
+        my_new_title = request.POST.get('title')
+        print(my_new_title)
+        # Product.objects.create(title=my_new_title)
+    context = {}
     return render(request, "products/product_create.html", context)
+
+# def product_create_view(request):
+#     form = ProductForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         form=ProductForm()
+
+#     context = {
+#         'form': form
+#     }
+#     return render(request, "products/product_create.html", context)
 
 def product_detail_view(request):
     obj = Product.objects.get(id=1)

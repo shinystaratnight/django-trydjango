09f1a109b7a27cbdc0da24f935bf94ff0eaee49f
Rising Star <star-atnight@outlook.com>
2019-12-15 17:46:29 +0800

    Add ModelForm

--- products/views.py
+++ products/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
-
+from .forms import ProductForm
 from .models import Product
+
+def product_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form=ProductForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, "products/product_create.html", context)
 
 def product_detail_view(request):
     obj = Product.objects.get(id=1)
     context = {
-        'title': obj.title,
-        'description': obj.description
+        'object': obj
     }
-
-    return render(request, "products/detail.html", context)
+    return render(request, "products/product_detail.html", context)

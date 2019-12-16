3a9c09f7eee7ed71b97be69563d699a082e11a53
Rising Star <star-atnight@outlook.com>
2019-12-16 11:05:21 +0800

    add raw detail class based view

--- courses/urls.py
+++ courses/urls.py
@@ -8,4 +8,5 @@ app_name = 'courses'
 urlpatterns = [
     path('', CourseView.as_view(template_name="contact.html"), name='courses-list'),
     # path('', my_fbv, name='courses-list'),
+    path('<int:id>/', CourseView.as_view(), name="course-detail'"),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,16 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
+
+from .models import Course
 
 # BASE VIEW Class = VIEW
 
 class CourseView(View):
-    template_name = "about.html"
-    def get(self, request, *args, **kwargs):
+    template_name = "courses/course_detail.html"
+    def get(self, request, id=None, *args, **kwargs):
         # GET method
-        return render(request, self.template_name, {})
+        context = {}
+        if id is not None:
+            obj = get_object_or_404(Course, id=id)
+            context['object'] = obj
+        return render(request, self.template_name, context)
 
-    def post(self, request, *args, **kwargs):
-        return render(request, 'about.html', {})
+    # def post(self, request, *args, **kwargs):
+    #     return render(request, 'about.html', {})
 
 # HTTP METHODS 
 def my_fbv(request, *args, **kwargs):

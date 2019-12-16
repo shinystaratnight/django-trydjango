98afad6a48553ff8dcd2dc56cfcd730906fdb95d
Rising Star <star-atnight@outlook.com>
2019-12-16 10:52:50 +0800

    add conversion from function based view to class based view

---
+++ courses/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import (
+    CourseView,
+    # my_fbv
+)
+
+app_name = 'courses'
+urlpatterns = [
+    path('', CourseView.as_view(template_name="contact.html"), name='courses-list'),
+    # path('', my_fbv, name='courses-list'),
+]
---
+++ courses/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.views import View
+
+# BASE VIEW Class = VIEW
+
+class CourseView(View):
+    template_name = "about.html"
+    def get(self, request, *args, **kwargs):
+        # GET method
+        return render(request, self.template_name, {})
+
+    def post(self, request, *args, **kwargs):
+        return render(request, 'about.html', {})
+
+# HTTP METHODS 
+def my_fbv(request, *args, **kwargs):
+    print(request.method)
+    return render(request, 'about.html', {})
